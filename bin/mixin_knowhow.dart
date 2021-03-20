93b5d70595c6fd914e41e2908eb72024d8a3037a
Bansook Nam <bs.nam@lawfully.com>
2021-03-20 10:23:16 +0900

    first commit

---
+++ bin/mixin_knowhow.dart
@@ -0,0 +1,5 @@
+import 'package:mixin_knowhow/mixin_knowhow.dart' as mixin_knowhow;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mixin_knowhow.calculate()}!');
+}
---
+++ test/mixin_knowhow_test.dart
@@ -0,0 +1,8 @@
+import 'package:mixin_knowhow/mixin_knowhow.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
